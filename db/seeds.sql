a86212d8d1d629f394a461f47e3d9561228091dc
Carson Curry <carsoncurry52@gmail.com>
2018-11-17 23:20:14 -0600

    changes for submission?

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('New Bacon-Ings');
+INSERT INTO burgers (burger_name) VALUES ('50 Ways to Leave Your Guava');
+INSERT INTO burgers (burger_name) VALUES ('Sweet Home Avocado');
+INSERT INTO burgers (burger_name) VALUES ('Hit Me With Your Best Shallot');
+INSERT INTO burgers (burger_name) VALUES ('Poblano Picasso');
